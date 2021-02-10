4b4522e3bad38e1210badfe4cb4647b7a095d651
JavaCode7 <76915005+JavaCode7@users.noreply.github.com>
2021-02-10 17:17:50 +0000

    Add Key bindings

--- src/nosweat.py
+++ src/nosweat.py
@@ -8,7 +8,7 @@ filename: str = ""
 window = Tk(className = " Text Editor")
 
 # Functions for Menubar
-def NSSave():
+def NSSave(*args):
     try:
         global filename
         with open(filename, "w") as fn:
@@ -16,7 +16,7 @@ def NSSave():
             fn.write(data)
     except FileNotFoundError:
         NSSaveAs()
-def NSSaveAs():
+def NSSaveAs(*args):
     file = filedialog.asksaveasfile(mode="w", defaultextension="*.tim", filetypes=(("Thulium 2 (*.tim)", "*.tim"), ("Thulium 1 (*.tlm)", "*.tlm"), ("Generic Text (*.txt)", "*.txt"), ("All files", "*.*")))
     global filename
     filename = file.name
@@ -25,11 +25,11 @@ def NSSaveAs():
         data = textArea.get("1.0", END+"-1c")
         file.write(data)
         file.close()
-def NSNew():
+def NSNew(*args):
     global filename
     filename = "Untitled"
     textArea.delete("1.0", END+"-1c")
-def NSOpen():
+def NSOpen(*args):
     global filename
     file = filedialog.askopenfile(parent=window, title="No Sweat: Open file", filetypes=(("Thulium 2 (*.tim)", "*.tim"), ("Thulium 1 (*.tlm)", "*.tlm"), ("Generic Text (*.txt)", "*.txt"), ("All files", "*.*")))
     filename = file.name
@@ -41,9 +41,9 @@ def NSOpen():
         file.close()
 def NSAbout():
     label = messagebox.showinfo("About", "No Sweat (or NSCode) is a simple editor made with python.")
-def NSRun2():
+def NSRun2(*args):
     system(f"thulium run {filename} a.tlmc")
-def NSRun1():
+def NSRun1(*args):
     system(f"thulium execute {filename}")
 
 
@@ -57,22 +57,31 @@ window.config(menu=menu)
 filemenu = Menu(menu)
 runmenu = Menu(menu)
 menu.add_cascade(label="File", menu=filemenu)
-filemenu.add_command(label="New", command=NSNew)
-filemenu.add_command(label="Open", command=NSOpen)
-filemenu.add_command(label="Save", command=NSSave)
-filemenu.add_command(label="Save As", command=NSSaveAs)
+filemenu.add_command(label="New                (Ctrl-N)", command=NSNew)
+filemenu.add_command(label="Open              (Ctrl-O)", command=NSOpen)
+filemenu.add_command(label="Save                (Ctrl-S)", command=NSSave)
+filemenu.add_command(label="Save As           (Ctrl-Shift-S)", command=NSSaveAs)
 filemenu.add_separator()
 filemenu.add_command(label="Exit", command=window.quit)
+
+# Running files
+menu.add_cascade(label="Run", menu=runmenu)
+runmenu.add_command(label="Thulium 1     (Ctrl-Shift-R)", command=NSRun1)
+runmenu.add_command(label="Thulium 2     (Ctrl-R)", command=NSRun2)
+
 
 # Help menu
 helpMenu = Menu(menu)
 menu.add_cascade(label="Help")
 menu.add_cascade(label="About", command=NSAbout)
 
-# Running files
-menu.add_cascade(label="Run", menu=runmenu)
-runmenu.add_command(label="Thulium 1", command=NSRun1)
-runmenu.add_command(label="Thulium 2", command=NSRun2)
+# Keyboard Bindings
+textArea.bind("<Control-o>", NSOpen)
+textArea.bind("<Control-n>", NSNew)
+textArea.bind("<Control-s>", NSSave)
+textArea.bind('<Control-Shift-KeyPress-S>', NSSaveAs)
+textArea.bind("<Control-r>", NSRun2)
+textArea.bind('<Control-Shift-KeyPress-R>', NSRun1)
 
 # More Tk stuff
 textArea.pack()
